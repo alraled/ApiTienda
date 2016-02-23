1e0e6b331bba449e3d5ab6fdf8d0737d7235fbda
alvaro <alraled@hotmail.es>
2016-02-23 09:28:13 +0100

    ApiTienda

---
+++ Repositorio/ViewModel/IViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Repositorio.ViewModel
+{
+    public interface IViewModel<TModelo> where TModelo : class
+    {
+
+        TModelo ToBaseDatos();
+
+        void FromBaseDatos(TModelo modelo);
+
+        void UpdateBaseDatos(TModelo modelo);
+
+        object[] GetKeys();
+
+
+
+    }
+
+
+
+
+}
---
+++ Repositorio/ViewModel/ViewModelAlmacen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositorio.Model;
+
+namespace Repositorio.ViewModel
+{
+    public class ViewModelAlmacen : IViewModel<Almacen>
+    {
+
+
+        public string Ciudad { get; set; }
+        public int CP { get; set; }
+        public int idAlmacen { get; set; }
+
+
+
+
+        public Almacen ToBaseDatos()
+        {
+            var al = new Almacen()
+            {
+                Ciudad = Ciudad,
+                CP = CP
+            };
+            return al;
+        }
+
+        public void FromBaseDatos(Almacen modelo)
+        {
+            Ciudad = modelo.Ciudad;
+            CP = modelo.CP;
+        }
+
+        public void UpdateBaseDatos(Almacen modelo)
+        {
+            modelo.Ciudad = Ciudad;
+            modelo.CP = CP;
+        }
+
+        public object[] GetKeys()
+        {
+            return new[] {Ciudad};
+        }
+    }
+}
